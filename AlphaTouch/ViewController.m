30900c30c6d3a3ddd37afb9ae13f1d253eacc77d
Andrew Gerst <gerst20051@gmail.com>
2013-08-02 15:18:06 -0400

    Initial Commit

---
+++ AlphaTouch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AlphaTouch
+//
+//  Created by gerst on 8/2/13.
+//  Copyright (c) 2013 gerst. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
